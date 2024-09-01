aeafc8732994851b3713671553456c322f511301
JoaoAugustoMV <93055242+JoaoAugustoMV@users.noreply.github.com>
2024-08-31 21:29:24 -0300

    Make fields nullable

--- app/models/informationDayForecast.py
+++ app/models/informationDayForecast.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class InformationDayForecast(BaseModel):
     id: str = None
     cd_dia: int
     x_dias: int = None
     dia_previsao_feita_menos_x: str = None
-    temperatura_min_previsao_feita_menos_x: int = None
-    temperatura_max_previsao_feita_menos_x: int = None
+    temperatura_min_previsao_feita_menos_x: Optional[int] = Field(default=None)
+    temperatura_max_previsao_feita_menos_x: Optional[int] = Field(default=None)
     
-    temperatura_real_min: int = None
-    temperatura_real_max: int = None
+    temperatura_real_min: Optional[int] = Field(default=None)
+    temperatura_real_max: Optional[int] = Field(default=None)
     
     fonte: str
     cidade: str
